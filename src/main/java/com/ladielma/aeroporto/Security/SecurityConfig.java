25770a350ee634834425c067bbbdfb9620627020
deLoretoMelo <meloloretomarcelo@gmail.com>
2025-03-11 22:09:23 -0300

    iniciado a criação de páginas de login e signup

---
+++ src/main/java/com/ladielma/aeroporto/Security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.ladielma.aeroporto.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // Função que configura as regras de acesso para a página de login
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                // Define como os usuários vao logar no sistema
+                .formLogin(formLogin -> {
+                    formLogin.loginPage("/login").permitAll();
+                    ;
+                })
+                // Define as regras de acesso para as partes da aplicação
+                .authorizeHttpRequests(authorizeRequests -> {
+                    authorizeRequests.requestMatchers("/signup", "/styles/**", "/js/**").permitAll();
+                    authorizeRequests.anyRequest().authenticated();
+
+                })
+                .build();
+    }
+}
